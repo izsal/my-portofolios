b4bdcee9b83517124c4257cdd75b6d671f79f524
Izsal Qurlinas Afandi <izsalavandi@gmail.com>
2023-08-12 17:25:35 +0700

    update portofilio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,28 +5,31 @@ const Footer = () => {
     <>
       <div className="footer-container">
         <p>© {new Date().getFullYear()} Izsal's Portfolio</p>
-        <div className="social-icons">
+        <div className="social-icons p-10">
           <a
-            href="https://twitter.com/olawanle_izsal"
+            href="https://twitter.com/Afandi_Qur"
             aria-label="Twitter"
             target="_blank"
             rel="noopener noreferrer"
+            className="mr-5"
           >
             <i className="fa-brands fa-twitter"></i>
           </a>
           <a
-            href="https://github.com/olawanleizsal"
+            href="https://github.com/izsal"
             aria-label="GitHub"
             target="_blank"
             rel="noopener noreferrer"
+            className="mr-5"
           >
             <i className="fa-brands fa-github"></i>
           </a>
           <a
-            href="https://www.linkedin.com/in/olawanleizsal/"
+            href="https://www.linkedin.com/in/izsal/"
             aria-label="LinkedIn"
             target="_blank"
             rel="noopener noreferrer"
+            className="mr-5"
           >
             <i className="fa-brands fa-linkedin"></i>
           </a>
--- src/components/data.js
+++ src/components/data.js
@@ -3,48 +3,61 @@
 export const projectData = [
   {
     id: 1,
-    title: 'React Crud',
+    title: "React Crud",
     description:
-      'A simple Crud App built with JavaScript. All datas are stored in localstorage. It helps users to make create, update and delete data ',
-    gitHubLink: 'https://github.com/izsal/react_crud',
+      "A simple Crud App built with JavaScript. All datas are stored in localstorage. It helps users to make create, update and delete data ",
+    gitHubLink: "https://github.com/izsal/react_crud",
   },
   {
     id: 2,
-    title: 'Social Media',
+    title: "Social Media",
     description:
-      'A simple Social Media App built with JavaScript. It helps users to make post in socmed with coment and may users',
-    gitHubLink: 'https://github.com/izsal/Fullstack-socmed-FrontEnd',
+      "A simple Social Media App built with JavaScript. It helps users to make post in socmed with coment and may users",
+    gitHubLink: "https://github.com/izsal/Fullstack-socmed-FrontEnd",
   },
   {
     id: 3,
-    title: 'Footbal data and Score',
-    description:
-      'Football live score and provisional scoreboard',
-    gitHubLink: 'https://github.com/izsal/Footbal-data-PWA.github.io',
+    title: "Footbal data and Score",
+    description: "Football live score and provisional scoreboard",
+    gitHubLink: "https://github.com/izsal/Footbal-data-PWA.github.io",
   },
   {
     id: 4,
-    title: 'Laravel Backend',
+    title: "Laravel Backend",
     description:
-      'backend app for manage person app frontend, and laravel as a backend to send data update data or delete data person in front end person',
-    gitHubLink: 'https://github.com/izsal/laravel_backend',
+      "backend app for manage person app frontend, and laravel as a backend to send data update data or delete data person in front end person",
+    gitHubLink: "https://github.com/izsal/laravel_backend",
   },
   {
-    id:5,
-    title: 'E-office',
-    description: 'web app for incoming and outgoing mail with PHP MVC Framework, and use for my company at RunSystem',
-    gitHubLink: "https://gitlab.com/izsalafandi/e-office"
+    id: 5,
+    title: "E-office",
+    description:
+      "web app for incoming and outgoing mail with PHP MVC Framework, and use for my company at RunSystem",
+    gitHubLink: "https://gitlab.com/izsalafandi/e-office",
   },
   {
-    id:6,
-    title: 'Fullstack socmed FrontEnd similar like twitter concept ',
-    description: 'web app socmed inspired by twitter. ',
-    gitHubLink: "https://github.com/izsal/Fullstack-socmed-FrontEnd"
+    id: 6,
+    title: "Fullstack socmed FrontEnd similar like twitter concept ",
+    description: "web app socmed inspired by twitter. ",
+    gitHubLink: "https://github.com/izsal/Fullstack-socmed-FrontEnd",
   },
   {
-    id:7,
-    title: 'Fullstack socmed BackEnd similar like twitter concept ',
-    description: 'web app socmed inspired by twitter. ',
-    gitHubLink: "https://github.com/izsal/fullstack-socmed-backend"
+    id: 7,
+    title: "Fullstack socmed BackEnd similar like twitter concept ",
+    description: "web app socmed inspired by twitter. ",
+    gitHubLink: "https://github.com/izsal/fullstack-socmed-backend",
+  },
+  {
+    id: 8,
+    title: "ECommerce Admin",
+    description:
+      "E-Commerce With CMS For database schema with prisma and use MYSQL . ",
+    gitHubLink: "https://github.com/izsal/next13-ecommerece-admin",
+  },
+  {
+    id: 8,
+    title: "ECommerce Store",
+    description: "E-Commerce Store to show data from ecommerce-admin. ",
+    gitHubLink: "https://github.com/izsal/next13-ecommerece-store",
   },
 ];
